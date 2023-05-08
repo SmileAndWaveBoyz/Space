45f991a71eb123b5b792e5b1a9809c774ce61874
Nathan Caney <nathancaney@gmail.com>
2023-05-08 23:48:39 +0100

    More spelling

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -42,7 +42,7 @@ function Crew() {
     
       case 2:
         setTitleHeading("PILOT");
-        setCrewHeading("VICTOR");
+        setCrewHeading("VICTOR GLOVER");
         setInfoTextCrew("Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.");
         break;
     
