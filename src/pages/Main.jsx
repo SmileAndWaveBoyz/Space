f55c368049249477524bb0d3d9c8db38becc2b34
Nathan Caney <nathancaney@gmail.com>
2023-05-08 23:35:11 +0100

    Spelling

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -12,7 +12,7 @@ function Main() {
       <div className="texBox">
           <h5 className='firstHeading'>SO, YOU WANT TO TRAVEL TO</h5>
           <h1 className='spaceHeading'>SPACE</h1>
-          <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+          <p>Let's face it, if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well, sit back and relax, because we'll give you a truly out of this world experience!</p>
       </div>
       <div className="explore" onClick={handleClick}>
           <p><a>EXPLORE</a></p>
