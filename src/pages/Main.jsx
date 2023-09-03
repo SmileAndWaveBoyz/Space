020a6d8fdc60ba52b0dea1c81e35514bf3337c60
Nathan Caney <nathancaney@gmail.com>
2023-09-03 20:34:32 +0100

    Outline on button fix

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,12 +9,12 @@ function Main() {
 
   return (
     <main className='hero'>
-      <div className="texBox">
+      <div className="texBox" data-aos="fade-up">
           <h5 className='firstHeading'>SO, YOU WANT TO TRAVEL TO</h5>
           <h1 className='spaceHeading'>SPACE</h1>
           <p>Let's face it, if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well, sit back and relax, because we'll give you a truly out of this world experience!</p>
       </div>
-      <div className="explore" onClick={handleClick}>
+      <div className="explore"  onClick={handleClick}>
           <p><a>EXPLORE</a></p>
       </div>
     </main> 
