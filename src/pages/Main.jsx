e013c59a9803402689b97f0aa13ca0cbdeb75f0f
Nathan Caney <nathancaney@gmail.com>
2023-09-05 17:21:29 +0100

    Edit home page par

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -12,7 +12,7 @@ function Main() {
       <div className="texBox" data-aos="fade-up">
           <h5 className='firstHeading'>SO, YOU WANT TO TRAVEL TO</h5>
           <h1 className='spaceHeading'>SPACE</h1>
-          <p>Let's face it, if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well, sit back and relax, because we'll give you a truly out of this world experience!</p>
+          <p>In the beginning the Universe was created. This had made many people very angry and has been widely regarded as a bad move.</p>
       </div>
       <div className="explore"  onClick={handleClick}>
           <p><a>EXPLORE</a></p>
