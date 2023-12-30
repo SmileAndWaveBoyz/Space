eee24976baa049151a81bbf716f69848adee50ce
SmileAndWaveBoyz <44876348+SmileAndWaveBoyz@users.noreply.github.com>
2023-12-30 19:16:25 +0000

    More spelling

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -12,7 +12,7 @@ function Main() {
       <div className="texBox" data-aos="fade-up">
           <h5 className='firstHeading'>SO, YOU WANT TO TRAVEL TO</h5>
           <h1 className='spaceHeading'>SPACE</h1>
-          <p>In the beginning the Universe was created. This has made many people very angry and has been widely regarded as a bad move.</p>
+          <p>In the beginning the universe was created. This has made many people very angry and has been widely regarded as a bad move.</p>
       </div>
       <div className="explore"  onClick={handleClick}>
           <p><a>EXPLORE</a></p>
